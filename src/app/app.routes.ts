00b370ac4a00decf920ef720c27dc78ecf049728
Henrik Sørensen <hesor@cs.aau.dk>
2025-04-03 00:12:48 +0200

    add examples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GridComponent } from './grid/grid.component';
+import { FlexComponent } from './flex/flex.component';
+import { TailwindComponent } from './tailwind/tailwind.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'grid', component: GridComponent},
+    { path: 'flex', component: FlexComponent},
+    { path: 'tailwind', component: TailwindComponent},
+];
---
+++ src/app/flex/flex.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flex',
+  imports: [],
+  templateUrl: './flex.component.html',
+  styleUrl: './flex.component.css'
+})
+export class FlexComponent {
+
+}
