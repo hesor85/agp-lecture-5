022f253a8d39e9217f9e5ff198eccaaf859491fa
Henrik Sørensen <hesor@cs.aau.dk>
2025-04-08 08:50:19 +0200

    New tv-shows component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { GridComponent } from './grid/grid.component';
 import { FlexComponent } from './flex/flex.component';
 import { TailwindComponent } from './tailwind/tailwind.component';
+import { TvShowsComponent } from './tv-shows/tv-shows.component';
 
 export const routes: Routes = [
-    { path: 'grid', component: GridComponent},
-    { path: 'flex', component: FlexComponent},
-    { path: 'tailwind', component: TailwindComponent},
+  { path: 'grid', component: GridComponent },
+  { path: 'flex', component: FlexComponent },
+  { path: 'tailwind', component: TailwindComponent },
+  { path: 'tvshows', component: TvShowsComponent },
 ];
